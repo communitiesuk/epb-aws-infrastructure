bcd9bcec89e73e0e508948636aff853a7d45a654
Eleanor Mollett <eleanor.mollett@gmail.com>
2023-01-31 11:48:18 +0000

    EPBR-2887: feat: Adds a security group and rules to the VPC

--- vpc.tf
+++ vpc.tf
@@ -4,3 +4,30 @@ resource "aws_vpc" "main" {
     Name = "epbr-${var.environment}"
   }
 }
+
+resource "aws_security_group" "internet_access" {
+  name        = "internet-access-${var.environment}"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    #    Terraform docs say the cidr_blocks are optional, however you can't see the rules being applied in the console without them
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    #    Terraform docs say the cidr_blocks are optional, however you can't see the rules being applied in the console without them
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "internet-access-${var.environment}"
+  }
+}
