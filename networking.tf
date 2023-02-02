361078cb8b539a9b1b14b8390482ed9f3d1f5818
Eleanor Mollett <eleanor.mollett@gmail.com>
2023-02-02 11:42:04 +0000

    EPBR-2874: feat: Add route tables for public subnet

--- networking.tf
+++ networking.tf
@@ -43,3 +43,19 @@ resource "aws_subnet" "private" {
   }
 }
 
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+}
+
+resource "aws_route" "public" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.this.id
+}
+
+resource "aws_route_table_association" "public" {
+  count          = length(aws_subnet.public)
+  subnet_id      = element(aws_subnet.public.*.id, count.index)
+  route_table_id = aws_route_table.public.id
+}
+
