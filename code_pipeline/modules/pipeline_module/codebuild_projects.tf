207ca17353676f2c00cf365b845781dd0d7d7966
Barry Halper <69764062+barryhalper@users.noreply.github.com>
2023-02-15 11:45:56 +0000

    EPBR-2877: feat: added environment variables to code build stage

--- code_pipeline/modules/pipeline_module/codebuild_projects.tf
+++ code_pipeline/modules/pipeline_module/codebuild_projects.tf
@@ -2,6 +2,13 @@ data "aws_ecr_repository" "codebuild_image" {
   name = "epbr-codebuild-cloudfoundry"
 }
 
+data "aws_ecr_repository" "postgres_image" {
+  name = "epbr-postgres"
+}
+
+data "aws_caller_identity" "current" {}
+
+
 
 resource "aws_codebuild_project" "build_and_test" {
   name         = "${var.project_name}-codebuild-build-and-test"
@@ -17,10 +24,32 @@ resource "aws_codebuild_project" "build_and_test" {
     image           =  data.aws_ecr_repository.codebuild_image.repository_url
     type            = "LINUX_CONTAINER"
     privileged_mode = true
+
+    environment_variable {
+      name  = "DOCKER_IMAGE_URI"
+      value = data.aws_ecr_repository.codebuild_image.repository_url
+    }
+
+    environment_variable {
+      name  = "POSTGRES_IMAGE_URL"
+      value = data.aws_ecr_repository.postgres_image.repository_url
+    }
+
+    environment_variable {
+      name  = "AWS_ACCOUNT_ID"
+      value = data.aws_caller_identity.current.account_id
+    }
+
+    environment_variable {
+      name  = "STAGE"
+      value = "integration"
+    }
+
   }
 
   source {
     type = "CODEPIPELINE"
+    buildspec = "buildspec/build_and_test_migration.yml"
   }
 }
 
--- code_pipeline/modules/pipeline_module/outputs.tf
+++ code_pipeline/modules/pipeline_module/outputs.tf
@@ -2,3 +2,4 @@ output "aws_codepipeline_arn" {
   value       = aws_codepipeline.codepipeline.arn
   description = "The arn of the code pipeline"
 }
+
