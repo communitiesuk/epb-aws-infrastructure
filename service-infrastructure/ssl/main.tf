e3f4d08d803d01cd3c371ed47b92bce996cf9755
Chirag Bhatti <chirag.bhatti@softwire.com>
2023-03-07 15:42:04 +0000

    EPBR-3077: feat: Declare terraform provider and move certificate implementation

--- service-infrastructure/ssl/main.tf
+++ service-infrastructure/ssl/main.tf
@@ -1,4 +1,10 @@
-resource "aws_acm_certificate" "this" {
-  domain_name       = "*.centraldatastore.net"
-  validation_method = "DNS"
+terraform {
+  required_version = "~>1.3"
+
+  required_providers {
+    aws = {
+      version = "~>4.0"
+      source  = "hashicorp/aws"
+    }
+  }
 }
