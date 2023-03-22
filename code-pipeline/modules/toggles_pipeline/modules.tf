25b9993bb86d8edfb3b978021ff8ad4f7f44e6c6
Eleanor Mollett <eleanor.mollett@gmail.com>
2023-03-22 13:13:03 +0000

    EPBR-2900 Add toggles pipeline

---
+++ code-pipeline/modules/toggles_pipeline/modules.tf
@@ -0,0 +1,35 @@
+
+data "aws_caller_identity" "current" {}
+
+module "codebuild_build_app_image" {
+  source             = "../codebuild_project"
+  codebuild_role_arn = var.codebuild_role_arn
+  name               = "${var.project_name}-codebuild-build-image"
+  buildspec_file     = "buildspec/build_docker_image.yml"
+  environment_type   = "ARM_CONTAINER"
+  build_image_uri    = var.aws_arm_codebuild_image
+  environment_variables = [
+    { name = "AWS_DEFAULT_REGION", value = var.region },
+    { name = "AWS_ACCOUNT_ID", value = var.account_ids["integration"] },
+    { name = "DOCKER_IMAGE_URI", value = "${var.account_ids["integration"]}.dkr.ecr.${var.region}.amazonaws.com/${var.app_ecr_name}" },
+  ]
+  region = var.region
+}
+
+
+module "codebuild_deploy_integration" {
+  source             = "../codebuild_project"
+  codebuild_role_arn = var.codebuild_role_arn
+  name               = "${var.project_name}-codebuild-deploy-integration"
+  environment_type   = "ARM_CONTAINER"
+  build_image_uri    = var.aws_arm_codebuild_image
+  buildspec_file     = "buildspec/deploy_to_cluster.yml"
+  environment_variables = [
+    { name = "AWS_DEFAULT_REGION", value = var.region },
+    { name = "AWS_ACCOUNT_ID", value = var.account_ids["integration"] },
+    { name = "DOCKER_IMAGE_URI", value = "${var.account_ids["integration"]}.dkr.ecr.${var.region}.amazonaws.com/${var.app_ecr_name}" },
+    { name = "CLUSTER_NAME", value = var.ecs_cluster_name },
+    { name = "SERVICE_NAME", value = var.ecs_service_name },
+  ]
+  region = var.region
+}
