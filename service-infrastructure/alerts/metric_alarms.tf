e6daf3f14e155bdd729eac1c9bd2509600322e62
LukaszDygon <lukasz.dygon@softwire.com>
2023-04-04 15:46:26 +0100

    EPBR-2917 feat: Add ALB 5xx error alerts

--- service-infrastructure/alerts/metric_alarms.tf
+++ service-infrastructure/alerts/metric_alarms.tf
@@ -115,16 +115,44 @@ resource "aws_cloudwatch_metric_alarm" "rds_cluster_cpu_usage" {
   ]
 }
 
-# Create a CloudWatch alarm for ALB 4xx errors
+# Create a CloudWatch alarm for ALB 5xx errors
+resource "aws_cloudwatch_metric_alarm" "rds_alb_5xx_errors" {
+  for_each = var.albs
+
+  alarm_name          = "${each.value}-5xx-errors"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "HTTPCode_ELB_5XX_Count"
+  namespace           = "AWS/ApplicationELB"
+  period              = 300
+  statistic           = "Sum"
+  threshold           = 0
+
+  dimensions = {
+    LoadBalancer = each.value
+  }
+
+  alarm_actions = [
+    aws_sns_topic.cloudwatch_alerts.arn,
+  ]
+  ok_actions = [
+    aws_sns_topic.cloudwatch_alerts.arn,
+  ]
+  insufficient_data_actions = [
+    aws_sns_topic.cloudwatch_alerts.arn,
+  ]
+}
+
+# Create a CloudWatch alarm for ALB 5xx errors
 resource "aws_cloudwatch_metric_alarm" "rds_alb_4xx_errors" {
   for_each = var.albs
 
-  alarm_name          = "${each.value}-cpu-usage"
+  alarm_name          = "${each.value}-4xx-errors"
   comparison_operator = "GreaterThanThreshold"
   evaluation_periods  = 1
   metric_name         = "HTTPCode_ELB_4XX_Count"
   namespace           = "AWS/ApplicationELB"
-  period              = 60
+  period              = 300
   statistic           = "Sum"
   threshold           = 0
 
