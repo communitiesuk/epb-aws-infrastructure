85379f198e6a588f4188571ed8b2655c98a8ae24
Barry Halper <69764062+barryhalper@users.noreply.github.com>
2023-04-13 16:24:47 +0100

    EPBR-3233 updated enable_exec_command role policy to ecs_task_execution_role

--- service-infrastructure/application/ecs_roles.tf
+++ service-infrastructure/application/ecs_roles.tf
@@ -138,7 +138,7 @@ resource "aws_iam_role_policy" "firehose_put_record" {
 resource "aws_iam_role_policy" "enable_exec_command" {
   count = var.enable_execute_command == true ? 1 : 0
   name = "${var.prefix}-enable_exec_command"
-  role = aws_iam_role.ecs_task_role.id
+  role = aws_iam_role.ecs_task_execution_role.id
 
   policy = jsonencode(
     {
