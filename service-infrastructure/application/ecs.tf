bdf6d3e9a84e44935f22c494ba85586f79a85a2c
LukaszDygon <lukasz.dygon@softwire.com>
2023-04-17 17:35:00 +0100

    EPBR-3076: feat:increase memory for ecs tasks 1-2GB and CPU 256-512

--- service-infrastructure/application/ecs.tf
+++ service-infrastructure/application/ecs.tf
@@ -11,8 +11,8 @@ resource "aws_ecs_task_definition" "this" {
   family                   = "${var.prefix}-ecs-task"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  cpu                      = 256
-  memory                   = 1024
+  cpu                      = 512
+  memory                   = 2048
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
   task_role_arn            = aws_iam_role.ecs_task_role.arn
   container_definitions = jsonencode([
