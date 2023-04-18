5d46a7790ad562a98ac5deeec95de3f7a3012dbe
Barry Halper <69764062+barryhalper@users.noreply.github.com>
2023-04-18 11:04:57 +0100

    EPBR-3176 applied tflint to cc_tray

--- ci/modules/cc_tray/variable.tf
+++ ci/modules/cc_tray/variable.tf
@@ -1,16 +1,19 @@
 variable "app_name" {
   description = "name of the app file name downloaded from existing infrastructure"
   default     = "ccxml"
+  type = string
 }
 
 variable "bucket" {
   description = "The bucket that will contain the feed"
   default = "epbr-cc-tray"
+  type = string
 }
 
 variable "key" {
   description = "The key within the bucket that will contain the feed"
   default     = "cc.xml"
+  type = string
 }
 
 
