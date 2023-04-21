ce1e49cb0b5fe16997b848ae88bcd5eb2f4616b4
Barry Halper <69764062+barryhalper@users.noreply.github.com>
2023-04-21 14:08:31 +0100

    feat: Updated migration bucket policy to allow copying of files from existing aws account

--- service-infrastructure/data_migration_shared/bucket_roles.tf
+++ service-infrastructure/data_migration_shared/bucket_roles.tf
@@ -1,24 +1,48 @@
 # https://medium.com/tensult/copy-s3-bucket-objects-across-aws-accounts-e46c15c4b9e1
-resource "aws_s3_bucket_policy" "backup_bucket_role" {
-  bucket = aws_s3_bucket.this.id
+resource "aws_iam_policy" "copy_backup_bucket_role" {
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Sid =  "DelegateS3Access"
+        Sid =  "DelegateS3StagAccess"
         Effect = "Allow"
-        Principal = {
-          AWS = "arn:aws:iam::689681667086:root"
-        }
+
         Action = [
-          "s3:ListBucket",
-          "s3:GetObject"
+          "s3:PutObject",
+          "s3:GetObject",
+          "s3:ListBucketMultipartUploads",
+          "s3:AbortMultipartUpload",
+          "s3:PutObjectVersionAcl",
+          "s3:DeleteObject",
+          "s3:PutObjectAcl",
+          "s3:ListMultipartUploadParts"
         ]
         Resource = [
           aws_s3_bucket.this.arn,
           "${aws_s3_bucket.this.arn}/*",
         ]
+      },
+      {
+        "Effect": "Allow",
+        "Action": [
+          "s3:PutObject",
+          "s3:GetObject",
+          "s3:ListBucketMultipartUploads",
+          "s3:AbortMultipartUpload",
+          "s3:PutObjectVersionAcl",
+          "s3:DeleteObject",
+          "s3:PutObjectAcl",
+          "s3:ListMultipartUploadParts",
+          "s3:CreateMultipartUpload"
+        ],
+        "Resource": [
+          "arn:aws:s3:::epb-register-api-db-backup",
+          "arn:aws:s3:::epb-register-api-db-backup/*"
+        ]
       }
+
     ]
+
   })
 }
+
