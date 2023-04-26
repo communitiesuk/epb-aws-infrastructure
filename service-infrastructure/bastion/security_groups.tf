69ba5b5a20f11ebeb33b0cb81c37a91489049c28
LukaszDygon <lukasz.dygon@softwire.com>
2023-04-26 15:06:21 +0100

    NONE: fix: Remove unused bastion SSH Security Group rule

--- service-infrastructure/bastion/security_groups.tf
+++ service-infrastructure/bastion/security_groups.tf
@@ -2,13 +2,6 @@ resource "aws_security_group" "bastion" {
   name   = "bastion-security-group"
   vpc_id = var.vpc_id
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   egress {
     from_port   = 0
     to_port     = 0
