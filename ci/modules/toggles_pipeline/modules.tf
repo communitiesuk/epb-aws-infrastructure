c25c45dbc2e833641622466853fd465568fb8d68
Chirag Bhatti <chirag.bhatti@softwire.com>
2023-05-12 13:49:45 +0100

    EPBR-3174: style: Tidy up style

--- ci/modules/toggles_pipeline/modules.tf
+++ ci/modules/toggles_pipeline/modules.tf
@@ -1,9 +1,9 @@
 module "codebuild_build_app_image" {
-  source                     = "../codebuild_project"
-  codebuild_role_arn         = var.codebuild_role_arn
-  name                       = "${var.project_name}-codebuild-build-image"
-  buildspec_file             = "buildspec/build_docker_image.yml"
-  build_image_uri            = var.aws_codebuild_image
+  source             = "../codebuild_project"
+  codebuild_role_arn = var.codebuild_role_arn
+  name               = "${var.project_name}-codebuild-build-image"
+  buildspec_file     = "buildspec/build_docker_image.yml"
+  build_image_uri    = var.aws_codebuild_image
   environment_variables = [
     { name = "AWS_DEFAULT_REGION", value = var.region },
     { name = "AWS_ACCOUNT_ID", value = var.account_ids["integration"] },
@@ -12,11 +12,11 @@ module "codebuild_build_app_image" {
 }
 
 module "codebuild_deploy_integration" {
-  source                     = "../codebuild_project"
-  codebuild_role_arn         = var.codebuild_role_arn
-  name                       = "${var.project_name}-codebuild-deploy-integration"
-  build_image_uri            = var.aws_codebuild_image
-  buildspec_file             = "buildspec/deploy_to_cluster.yml"
+  source             = "../codebuild_project"
+  codebuild_role_arn = var.codebuild_role_arn
+  name               = "${var.project_name}-codebuild-deploy-integration"
+  build_image_uri    = var.aws_codebuild_image
+  buildspec_file     = "buildspec/deploy_to_cluster.yml"
   environment_variables = [
     { name = "AWS_DEFAULT_REGION", value = var.region },
     { name = "AWS_ACCOUNT_ID", value = var.account_ids["integration"] },
@@ -28,11 +28,11 @@ module "codebuild_deploy_integration" {
 }
 
 module "codebuild_deploy_staging" {
-  source                     = "../codebuild_project"
-  codebuild_role_arn         = var.codebuild_role_arn
-  name                       = "${var.project_name}-codebuild-deploy-staging"
-  build_image_uri            = var.aws_codebuild_image
-  buildspec_file             = "buildspec/deploy_to_cluster.yml"
+  source             = "../codebuild_project"
+  codebuild_role_arn = var.codebuild_role_arn
+  name               = "${var.project_name}-codebuild-deploy-staging"
+  build_image_uri    = var.aws_codebuild_image
+  buildspec_file     = "buildspec/deploy_to_cluster.yml"
   environment_variables = [
     { name = "AWS_DEFAULT_REGION", value = var.region },
     { name = "AWS_ACCOUNT_ID", value = var.account_ids["staging"] },
