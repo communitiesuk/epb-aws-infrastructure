85484bcc8f00c4b1776cd6300ed1141945d291de
Barry Halper <69764062+barryhalper@users.noreply.github.com>
2023-05-16 16:02:46 +0100

    EPRB-3251 feat:updated ci_role to enable run and describe task

--- service-infrastructure/access/ci_role.tf
+++ service-infrastructure/access/ci_role.tf
@@ -15,23 +15,7 @@ resource "aws_iam_role" "ci_role" {
   })
 }
 
-resource "aws_iam_role" "ci_task_role" {
-  name        = "ci-server-task-role"
-  description = "Used by a CI server operating from a separate account"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-          AWS     = "arn:aws:iam::${var.ci_account_id}:root"
-        }
-      }
-    ]
-  })
-}
+
 
 # https://docs.aws.amazon.com/AmazonECS/latest/userguide/security_iam_id-based-policy-examples.html#IAM_update_service_policies
 resource "aws_iam_role_policy" "ci_ecs_policy" {
@@ -44,7 +28,10 @@ resource "aws_iam_role_policy" "ci_ecs_policy" {
         Effect = "Allow"
         Action = [
           "ecs:UpdateService",
-          "ec2:Describe*"
+          "ec2:Describe*",
+          "ecs:RunTask",
+          "ecs:DescribeTasks",
+          "iam:PassRole"
         ]
         Resource = "*"
       }
