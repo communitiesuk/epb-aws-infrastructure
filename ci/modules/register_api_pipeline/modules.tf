9073837fbcfe76c02c3406e67738a7a197d15820
Chirag Bhatti <chirag.bhatti@softwire.com>
2023-05-30 15:09:05 +0100

    EPBR-2934: feat: Update buildspec file used for performance test on register api pipeline

--- ci/modules/register_api_pipeline/modules.tf
+++ ci/modules/register_api_pipeline/modules.tf
@@ -170,7 +170,7 @@ module "codebuild_performance_test" {
   codebuild_role_arn    = var.codebuild_role_arn
   name                  = "${var.project_name}-codebuild-performance-test"
   build_image_uri       = var.aws_codebuild_image
-  buildspec_file        = "buildspec.yml"
+  buildspec_file        = "buildspec_aws.yml"
   environment_variables = []
   region                = var.region
 }
