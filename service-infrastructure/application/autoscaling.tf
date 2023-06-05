8e6ed865803cecb05f60123f42e9262e6545c3d1
Chirag Bhatti <chirag.bhatti@softwire.com>
2023-06-05 17:41:10 +0100

    EPBR-2934: feat: Increase ecs autoscaling target min max capacity

--- service-infrastructure/application/autoscaling.tf
+++ service-infrastructure/application/autoscaling.tf
@@ -1,6 +1,6 @@
 resource "aws_appautoscaling_target" "ecs_target" {
-  max_capacity       = 2
-  min_capacity       = 1
+  max_capacity       = 4
+  min_capacity       = 2
   resource_id        = "service/${aws_ecs_cluster.this.name}/${aws_ecs_service.this.name}"
   scalable_dimension = "ecs:service:DesiredCount"
   service_namespace  = "ecs"
