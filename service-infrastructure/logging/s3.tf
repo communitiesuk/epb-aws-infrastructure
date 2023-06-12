6e8325fe6edfa3943af473ad88b92cf9cc8f3ce1
Alice Lui <54395483+al01010@users.noreply.github.com>
2023-06-12 16:43:31 +0100

    EPBR-3352: feat: Restrict ACLs to data migration, ode, and logging buckets

--- service-infrastructure/logging/s3.tf
+++ service-infrastructure/logging/s3.tf
@@ -3,9 +3,11 @@ resource "aws_s3_bucket" "logs" {
 }
 
 resource "aws_s3_bucket_public_access_block" "logs" {
-  bucket              = aws_s3_bucket.logs.id
-  block_public_acls   = true
-  block_public_policy = true
+  bucket                  = aws_s3_bucket.logs.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_ownership_controls" "example" {
--- service-infrastructure/open_data_export/s3.tf
+++ service-infrastructure/open_data_export/s3.tf
@@ -4,7 +4,9 @@ resource "aws_s3_bucket" "open_data_export" {
 }
 
 resource "aws_s3_bucket_public_access_block" "this" {
-  bucket              = aws_s3_bucket.open_data_export.id
-  block_public_acls   = true
-  block_public_policy = true
+  bucket                  = aws_s3_bucket.open_data_export.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
