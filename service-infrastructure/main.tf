6af8758c467e8f930ccd4f168953b2c5df68df3b
Douglas Greenshields <douglas.greenshields@levellingup.gov.uk>
2023-06-13 15:46:35 +0100

    EPBR-3099 feat: add Environment tag to all resources in service infrastructure module

--- service-infrastructure/main.tf
+++ service-infrastructure/main.tf
@@ -14,12 +14,22 @@ provider "aws" {
   region                   = "us-east-1"
   shared_config_files      = ["~/.aws/config"]
   shared_credentials_files = ["~/.aws/credentials"]
+  default_tags {
+    tags = {
+      Environment = var.environment
+    }
+  }
 }
 
 provider "aws" {
   region                   = "eu-west-2"
   shared_config_files      = ["~/.aws/config"]
   shared_credentials_files = ["~/.aws/credentials"]
+  default_tags {
+    tags = {
+      Environment = var.environment
+    }
+  }
 }
 
 locals {
