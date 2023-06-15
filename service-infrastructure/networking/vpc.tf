1f7104b3ff94e1549418ba6435c63a0dd5e853ac
Chirag Bhatti <chirag.bhatti@softwire.com>
2023-06-15 09:55:44 +0100

    Revert "Revert "EPBR-3365: feat: Add network acls for public and private subnets""
    This reverts commit 3a3dadce20ce8836f61538b766a73ecacb88fce9.

--- service-infrastructure/networking/vpc.tf
+++ service-infrastructure/networking/vpc.tf
@@ -45,3 +45,67 @@ resource "aws_db_subnet_group" "private_subnet_group" {
   name       = "${var.prefix}-private-subnet-group"
   subnet_ids = aws_subnet.private[*].id
 }
+
+resource "aws_network_acl" "public_subnets" {
+  vpc_id     = aws_vpc.this.id
+  subnet_ids = toset(aws_subnet.public[*].id)
+
+  ingress {
+    from_port  = 80
+    to_port    = 80
+    rule_no    = 100
+    action     = "allow"
+    protocol   = "tcp"
+    cidr_block = "0.0.0.0/0"
+  }
+
+  ingress {
+    from_port  = 443
+    to_port    = 443
+    rule_no    = 101
+    action     = "allow"
+    protocol   = "tcp"
+    cidr_block = "0.0.0.0/0"
+  }
+
+  ingress {
+    from_port  = 0
+    to_port    = 0
+    rule_no    = 102
+    action     = "allow"
+    protocol   = -1
+    cidr_block = var.vpc_cidr_block
+  }
+
+  egress {
+    from_port  = 0
+    to_port    = 0
+    rule_no    = 100
+    action     = "allow"
+    protocol   = -1
+    cidr_block = "0.0.0.0/0"
+  }
+}
+
+resource "aws_network_acl" "private_subnets" {
+  vpc_id     = aws_vpc.this.id
+  subnet_ids = toset(aws_subnet.private[*].id)
+
+  ingress {
+    from_port  = 0
+    to_port    = 0
+    rule_no    = 100
+    action     = "allow"
+    protocol   = -1
+    cidr_block = var.vpc_cidr_block
+  }
+
+  egress {
+    from_port  = 0
+    to_port    = 0
+    rule_no    = 100
+    action     = "allow"
+    protocol   = -1
+    cidr_block = "0.0.0.0/0"
+  }
+}
