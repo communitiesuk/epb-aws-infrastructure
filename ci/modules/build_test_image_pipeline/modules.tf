289446f3800a8606cf7a5bfcd5cc72e065001593
Chirag Bhatti <chirag.bhatti@softwire.com>
2023-06-20 10:20:18 +0100

    NONE: fix: Update buildspec file to use for build test image pipeline

--- ci/modules/build_test_image_pipeline/modules.tf
+++ ci/modules/build_test_image_pipeline/modules.tf
@@ -5,7 +5,7 @@ module "codebuild_build_push_image" {
   codebuild_role_arn = aws_iam_role.codebuild_role.arn
   name               = "epbr-codebuild-images-${var.configuration}-project"
   build_image_uri    = "aws/codebuild/standard:7.0"
-  buildspec_file     = "${var.configuration}/buildspec.yml"
+  buildspec_file     = "${var.configuration}/buildspec-aws.yml"
   environment_variables = [
     { name = "REPOSITORY_URI", value = aws_ecr_repository.this.repository_url },
     { name = "AWS_ACCOUNT_ID", value = data.aws_caller_identity.current.account_id },
