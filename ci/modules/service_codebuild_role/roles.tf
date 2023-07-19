60515dd054f087c85ddd81c6f4213c1556256053
Barry Halper <69764062+barryhalper@users.noreply.github.com>
2023-07-19 16:38:07 +0100

    EPBR-3479: fix ensure codebuild role match changes made in the console

--- ci/modules/service_codebuild_role/roles.tf
+++ ci/modules/service_codebuild_role/roles.tf
@@ -10,96 +10,96 @@ data "aws_iam_policy_document" "assume_role_codebuild" {
   }
 }
 
-data "aws_iam_policy_document" "codebuild_role_policy" {
-  statement {
-    effect = "Allow"
-    actions = [
-      "logs:CreateLogGroup",
-      "logs:CreateLogStream",
-      "logs:PutLogEvents"
-    ]
-    resources = [
-      "*"
-    ]
-  }
-
-  statement {
-    effect = "Allow"
-    actions = [
-      "ecr:GetDownloadUrlForLayer",
-      "ecr:BatchGetImage",
-      "ecr:BatchCheckLayerAvailability",
-      "ecr:DescribeRepositories",
-      "ecr:GetRepositoryPolicy",
-      "ecr:ListImages",
-      "ecr:PutImage",
-      "ecr:InitiateLayerUpload",
-      "ecr:UploadLayerPart",
-      "ecr:CompleteLayerUpload",
-      "ecr:GetAuthorizationToken",
-    ]
-    resources = ["*"]
-  }
-
-  statement {
-    effect = "Allow"
-    actions = [
-      "s3:PutObject",
-      "s3:GetObject",
-      "s3:GetObjectVersion",
-      "s3:GetBucketAcl",
-      "s3:GetBucketLocation"
-    ]
-    resources = [
-      var.codepipeline_bucket_arn,
-      "${var.codepipeline_bucket_arn}/*",
-      var.performance_reports_bucket_arn,
-      "${var.performance_reports_bucket_arn}/*",
-    ]
-  }
-
-  statement {
-    effect = "Allow"
-    actions = [
-      "s3:DeleteObject",
-      "s3:GetBucketLocation",
-      "s3:GetObject",
-      "s3:ListBucket",
-      "s3:PutObject"
-    ]
-    resources = ["arn:aws:s3:::${var.tech_docs_bucket_repo}", "arn:aws:s3:::${var.tech_docs_bucket_repo}/*"]
-  }
-
-  statement {
-    effect = "Allow"
-    actions = [
-      "codestar-connections:UseConnection",
-    ]
-    resources = [var.codestar_connection_arn]
-  }
-
-  statement {
-    effect = "Allow"
-    actions = [
-      "ssm:GetParameters"
-    ]
-    resources = ["*"]
-  }
-
-  statement {
-    effect    = "Allow"
-    actions   = ["sts:AssumeRole"]
-    resources = var.cross_account_role_arns
-  }
-}
-
 resource "aws_iam_role" "codebuild_role" {
   name               = "epbr-codebuild-role"
   assume_role_policy = data.aws_iam_policy_document.assume_role_codebuild.json
 }
 
 resource "aws_iam_role_policy" "codebuild_policy" {
-  name   = "epbr-codebuild-policy"
-  role   = aws_iam_role.codebuild_role.id
-  policy = data.aws_iam_policy_document.codebuild_role_policy.json
+  name = "epbr-codebuild-policy"
+  role = aws_iam_role.codebuild_role.id
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Action" : [
+          "logs:PutLogEvents",
+          "logs:CreateLogStream",
+          "logs:CreateLogGroup"
+        ],
+        "Effect" : "Allow",
+        "Resource" : "*",
+        "Sid" : ""
+      },
+      {
+        "Action" : [
+          "ecr:UploadLayerPart",
+          "ecr:PutImage",
+          "ecr:ListImages",
+          "ecr:InitiateLayerUpload",
+          "ecr:GetRepositoryPolicy",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:GetAuthorizationToken",
+          "ecr:DescribeRepositories",
+          "ecr:CompleteLayerUpload",
+          "ecr:BatchGetImage",
+          "ecr:BatchCheckLayerAvailability"
+        ],
+        "Effect" : "Allow",
+        "Resource" : "*",
+        "Sid" : ""
+      },
+      {
+        "Action" : [
+          "s3:PutObject",
+          "s3:GetObjectVersion",
+          "s3:GetObject",
+          "s3:GetBucketLocation",
+          "s3:GetBucketAcl"
+        ],
+        "Effect" : "Allow",
+        "Resource" : [
+          var.codepipeline_bucket_arn,
+          "${var.codepipeline_bucket_arn}/*",
+          var.performance_reports_bucket_arn,
+          "${var.performance_reports_bucket_arn}/*",
+        ],
+        "Sid" : ""
+      },
+      {
+        "Action" : [
+          "s3:GetLifecycleConfiguration",
+          "s3:ListBucket",
+          "s3:DeleteObject",
+          "s3:GetBucketLocation",
+          "s3:GetObject",
+          "s3:ListBucket",
+          "s3:PutObject",
+          "s3:PutObjectAcl"
+        ],
+        "Effect" : "Allow",
+        "Resource" : ["arn:aws:s3:::${var.tech_docs_bucket_repo}",
+        "arn:aws:s3:::${var.tech_docs_bucket_repo}/*"],
+        "Sid" : ""
+      },
+      {
+        "Action" : "codestar-connections:UseConnection",
+        "Effect" : "Allow",
+        "Resource" : [var.codestar_connection_arn],
+        "Sid" : ""
+      },
+      {
+        "Action" : "ssm:GetParameters",
+        "Effect" : "Allow",
+        "Resource" : "*",
+        "Sid" : ""
+      },
+      {
+        "Action" : "sts:AssumeRole",
+        "Effect" : "Allow",
+        "Resource" : [var.cross_account_role_arns],
+        "Sid" : ""
+      }
+    ]
+  })
 }
