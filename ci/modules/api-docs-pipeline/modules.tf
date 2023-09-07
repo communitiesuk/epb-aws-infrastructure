69f5066482bd65ab7da161bd26fa92d7ee78eccb
Douglas Greenshields <douglas.greenshields@levellingup.gov.uk>
2023-09-07 09:28:48 +0100

    EPBR-3480 ci: correct buildspec file reference for api docs

--- ci/modules/api-docs-pipeline/modules.tf
+++ ci/modules/api-docs-pipeline/modules.tf
@@ -3,7 +3,7 @@ module "codebuild_build_push_repo" {
   codebuild_role_arn = var.codebuild_role_arn
   name               = "epbr-codebuild-${var.project_name}"
   build_image_uri    = "aws/codebuild/standard:7.0"
-  buildspec_file     = "${var.configuration}/build_and_push_api_docs_to_aws.yml"
+  buildspec_file     = "${var.configuration}/build_api_docs_and_push_to_aws.yml"
   environment_variables = [
     { name = "BUCKET_NAME", value = var.repo_bucket_name },
     { name = "AWS_ACCOUNT_ID", value = var.dev_account_id },
