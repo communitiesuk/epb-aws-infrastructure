f849bb85b83f3d6d47f83666252dfa287be39f61
Eleanor Mollett <eleanor.mollett@gmail.com>
2023-10-18 10:56:23 +0100

    EPBR-4050 fix: Increasing deregistration delay to see if it impacts 5xx error rate

--- service-infrastructure/application/alb-internal.tf
+++ service-infrastructure/application/alb-internal.tf
@@ -30,7 +30,7 @@ resource "aws_lb_target_group" "internal" {
   protocol             = "HTTP"
   vpc_id               = var.vpc_id
   target_type          = "ip"
-  deregistration_delay = 30
+  deregistration_delay = 300
 
   health_check {
     interval            = "31"
--- service-infrastructure/application/front_door/alb.tf
+++ service-infrastructure/application/front_door/alb.tf
@@ -21,7 +21,7 @@ resource "aws_lb_target_group" "public" {
   protocol             = "HTTP"
   vpc_id               = var.vpc_id
   target_type          = "ip"
-  deregistration_delay = 30
+  deregistration_delay = 300
 
   health_check {
     interval            = "31"
@@ -42,7 +42,7 @@ resource "aws_lb_target_group" "extra" {
   protocol             = "HTTP"
   vpc_id               = var.vpc_id
   target_type          = "ip"
-  deregistration_delay = 30
+  deregistration_delay = 300
 
   health_check {
     interval            = "31"
