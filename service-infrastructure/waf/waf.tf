c1ced5c9e9a79c15aaa3b27af3209dc2dd609238
Eleanor Mollett <eleanor.mollett@gmail.com>
2023-11-16 11:27:54 +0000

    EPBR-4331 Add bot counting to WAF

--- service-infrastructure/waf/waf.tf
+++ service-infrastructure/waf/waf.tf
@@ -136,6 +136,33 @@ resource "aws_wafv2_web_acl" "this" {
     }
   }
 
+  rule {
+    name     = "AWS-AWSManagedRulesBotControlRuleSet"
+    priority = 14
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesBotControlRuleSet"
+        vendor_name = "AWS"
+        managed_rule_group_configs {
+          aws_managed_rules_bot_control_rule_set {
+            inspection_level = "COMMON"
+          }
+        }
+      }
+    }
+
+    override_action {
+      count {}
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "waf-block-bots"
+      sampled_requests_enabled   = true
+    }
+  }
+
   visibility_config {
     cloudwatch_metrics_enabled = true
     metric_name                = "waf-metrics"
