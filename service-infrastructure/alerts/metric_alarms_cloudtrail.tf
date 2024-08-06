4a1f7f1562b74fda5162b6aae1a07a7ab46c1b93
Eleanor Mollett <eleanor.mollett@gmail.com>
2024-08-06 12:11:42 +0100

    EPBR-3398 Set missing data as notBreaching on further alarms, and reduce period to alert us sooner.

--- service-infrastructure/alerts/metric_alarms_cloudtrail.tf
+++ service-infrastructure/alerts/metric_alarms_cloudtrail.tf
@@ -4,9 +4,10 @@ resource "aws_cloudwatch_metric_alarm" "unauthorized_api_calls_alarm" {
   evaluation_periods  = 1
   metric_name         = aws_cloudwatch_log_metric_filter.unauthorized_api_calls_metric.name
   namespace           = "CISBenchmark"
-  period              = 300
+  period              = 60
   statistic           = "Sum"
   threshold           = 1
+  treat_missing_data  = "notBreaching"
 
   alarm_actions = [
     aws_sns_topic.cloudwatch_alerts_cloudtrail.arn,
@@ -19,9 +20,10 @@ resource "aws_cloudwatch_metric_alarm" "no_mfa_console_signin_alarm" {
   evaluation_periods  = 1
   metric_name         = aws_cloudwatch_log_metric_filter.no_mfa_console_signin_metric.name
   namespace           = "CISBenchmark"
-  period              = 300
+  period              = 60
   statistic           = "Sum"
   threshold           = 1
+  treat_missing_data  = "notBreaching"
 
   alarm_actions = [
     aws_sns_topic.cloudwatch_alerts_cloudtrail.arn,
@@ -34,9 +36,10 @@ resource "aws_cloudwatch_metric_alarm" "root_account_login_alarm" {
   evaluation_periods  = 1
   metric_name         = aws_cloudwatch_log_metric_filter.root_account_login_metric.name
   namespace           = "CISBenchmark"
-  period              = 300
+  period              = 60
   statistic           = "Sum"
   threshold           = 1
+  treat_missing_data  = "notBreaching"
 
   alarm_actions = [
     aws_sns_topic.cloudwatch_alerts_cloudtrail.arn,
@@ -49,9 +52,10 @@ resource "aws_cloudwatch_metric_alarm" "iam_policy_changes_alarm" {
   evaluation_periods  = 1
   metric_name         = aws_cloudwatch_log_metric_filter.iam_policy_changes_metric.name
   namespace           = "CISBenchmark"
-  period              = 300
+  period              = 60
   statistic           = "Sum"
   threshold           = 1
+  treat_missing_data  = "notBreaching"
 
   alarm_actions = [
     aws_sns_topic.cloudwatch_alerts_cloudtrail.arn,
@@ -64,9 +68,10 @@ resource "aws_cloudwatch_metric_alarm" "cloudtrail_config_changes_alarm" {
   evaluation_periods  = 1
   metric_name         = aws_cloudwatch_log_metric_filter.cloudtrail_config_changes_metric.name
   namespace           = "CISBenchmark"
-  period              = 300
+  period              = 60
   statistic           = "Sum"
   threshold           = 1
+  treat_missing_data  = "notBreaching"
 
   alarm_actions = [
     aws_sns_topic.cloudwatch_alerts_cloudtrail.arn,
@@ -79,9 +84,10 @@ resource "aws_cloudwatch_metric_alarm" "s3_bucket_policy_changes_alarm" {
   evaluation_periods  = 1
   metric_name         = aws_cloudwatch_log_metric_filter.s3_bucket_policy_changes_metric.name
   namespace           = "CISBenchmark"
-  period              = 300
+  period              = 60
   statistic           = "Sum"
   threshold           = 1
+  treat_missing_data  = "notBreaching"
 
   alarm_actions = [
     aws_sns_topic.cloudwatch_alerts_cloudtrail.arn,
@@ -94,9 +100,10 @@ resource "aws_cloudwatch_metric_alarm" "network_gateway_changes_alarm" {
   evaluation_periods  = 1
   metric_name         = aws_cloudwatch_log_metric_filter.network_gateway_changes_metric.name
   namespace           = "CISBenchmark"
-  period              = 300
+  period              = 60
   statistic           = "Sum"
   threshold           = 1
+  treat_missing_data  = "notBreaching"
 
   alarm_actions = [
     aws_sns_topic.cloudwatch_alerts_cloudtrail.arn,
@@ -109,9 +116,10 @@ resource "aws_cloudwatch_metric_alarm" "route_tables_changes_alarm" {
   evaluation_periods  = 1
   metric_name         = aws_cloudwatch_log_metric_filter.route_tables_changes_metric.name
   namespace           = "CISBenchmark"
-  period              = 300
+  period              = 60
   statistic           = "Sum"
   threshold           = 1
+  treat_missing_data  = "notBreaching"
 
   alarm_actions = [
     aws_sns_topic.cloudwatch_alerts_cloudtrail.arn,
@@ -124,9 +132,10 @@ resource "aws_cloudwatch_metric_alarm" "vpc_changes_alarm" {
   evaluation_periods  = 1
   metric_name         = aws_cloudwatch_log_metric_filter.vpc_changes_metric.name
   namespace           = "CISBenchmark"
-  period              = 300
+  period              = 60
   statistic           = "Sum"
   threshold           = 1
+  treat_missing_data  = "notBreaching"
 
   alarm_actions = [
     aws_sns_topic.cloudwatch_alerts_cloudtrail.arn,
@@ -139,9 +148,10 @@ resource "aws_cloudwatch_metric_alarm" "organization_changes_alarm" {
   evaluation_periods  = 1
   metric_name         = aws_cloudwatch_log_metric_filter.organization_changes_metric.name
   namespace           = "CISBenchmark"
-  period              = 300
+  period              = 60
   statistic           = "Sum"
   threshold           = 1
+  treat_missing_data  = "notBreaching"
 
   alarm_actions = [
     aws_sns_topic.cloudwatch_alerts_cloudtrail.arn,
