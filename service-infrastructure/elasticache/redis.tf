48e661bd183b40f745daa861222c3c2c31e75da6
Eleanor Mollett <eleanor.mollett@gmail.com>
2024-08-28 09:04:34 +0100

    EPBR-6531 Making comment about redis engine group updates clearer
    When the parameter_groups file was deleted we lost some of the detail in the comments which will help when upgrading in future

--- service-infrastructure/elasticache/redis.tf
+++ service-infrastructure/elasticache/redis.tf
@@ -1,19 +1,21 @@
 resource "aws_elasticache_cluster" "redis" {
-  cluster_id           = "${var.prefix}-redis"
-  engine               = "redis"
-  node_type            = "cache.t3.micro"
-  num_cache_nodes      = 1
+  cluster_id = "${var.prefix}-redis"
+
+  engine = "redis"
+  # If updating the engine_version, ensure the version referenced for the parameter_group_name tallies
+  engine_version = "7.1"
+  # Parameter group families are outlined here - https://docs.aws.amazon.com/AmazonElastiCache/latest/red-ug/ParameterGroups.Redis.html
   parameter_group_name = "default.redis7"
 
+  node_type                  = "cache.t3.micro"
+  num_cache_nodes            = 1
   apply_immediately          = true
   auto_minor_version_upgrade = true
-  # If updating the engine_version, ensure the version referenced for the family attribute in the elasticache parameter group
-  engine_version           = "7.1"
-  port                     = var.redis_port
-  maintenance_window       = "mon:03:00-mon:07:00"
-  security_group_ids       = [aws_security_group.redis.id]
-  snapshot_retention_limit = 0
-  subnet_group_name        = aws_elasticache_subnet_group.this.name
+  port                       = var.redis_port
+  maintenance_window         = "mon:03:00-mon:07:00"
+  security_group_ids         = [aws_security_group.redis.id]
+  snapshot_retention_limit   = 0
+  subnet_group_name          = aws_elasticache_subnet_group.this.name
 
   log_delivery_configuration {
     destination      = var.aws_cloudwatch_log_group_name
