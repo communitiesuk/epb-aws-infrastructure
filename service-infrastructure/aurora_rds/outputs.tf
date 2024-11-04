f6dda204e536496096d4f1336961da31f59bf9bb
Barry Halper <69764062+barryhalper@users.noreply.github.com>
2024-11-04 15:55:14 +0000

    EPBR-7439 fix output for imported db
    When importing the db cluster into the state the output file raises an interpolation error. This is because the state does not at that point know about db instances and so cannot create a URL string to connect to them. This changed fixes that issue by adding flow control to the output.

--- service-infrastructure/aurora_rds/outputs.tf
+++ service-infrastructure/aurora_rds/outputs.tf
@@ -1,8 +1,7 @@
+
 output "rds_full_access_policy_arn" {
   value = aws_iam_policy.rds.arn
 }
-
-
 
 output "rds_cluster_identifier" {
   value = aws_rds_cluster.this.cluster_identifier
@@ -16,14 +15,21 @@ output "rds_db_username" {
   value = aws_rds_cluster.this.master_username
 }
 
+locals {
+  connection_string        = "postgresql://${aws_rds_cluster.this.master_username == null ? "" : aws_rds_cluster.this.master_username}:${aws_rds_cluster.this.master_password == null ? "" : aws_rds_cluster.this.master_password}@${aws_rds_cluster.this.endpoint == null ? "" : aws_rds_cluster.this.endpoint}/${aws_rds_cluster.this.database_name == null ? "" : aws_rds_cluster.this.database_name}"
+  reader_connection_string = replace(local.connection_string, aws_rds_cluster.this.endpoint, aws_rds_cluster.this.reader_endpoint)
+}
+
+
 output "rds_db_connection_string" {
   description = "A libpq (Postgresql) connection string for consuming this database, intended to be set as the environment variable DATABASE_URL"
-  value       = "postgresql://${aws_rds_cluster.this.master_username}:${aws_rds_cluster.this.master_password}@${aws_rds_cluster.this.endpoint}/${aws_rds_cluster.this.database_name}"
+  value       = local.connection_string
   sensitive   = true
 }
 
+
 output "rds_db_reader_connection_string" {
   description = "A libpq (Postgresql) connection string for consuming this database, intended to be set as the environment variable DATABASE_URL"
-  value       = "postgresql://${aws_rds_cluster.this.master_username}:${aws_rds_cluster.this.master_password}@${aws_rds_cluster.this.reader_endpoint}/${aws_rds_cluster.this.database_name}"
+  value       = local.reader_connection_string
   sensitive   = true
 }
