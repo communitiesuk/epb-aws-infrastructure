2958ec9d26cd495b9deb5478d5ba59923e470439
Alice Lui <54395483+al01010@users.noreply.github.com>
2024-12-18 11:14:58 +0000

    Temporarily turn off unauthorised api call alarm
    Whilst the wiz trail is taking place

--- service-infrastructure/alerts/metric_alarms_cloudtrail.tf
+++ service-infrastructure/alerts/metric_alarms_cloudtrail.tf
@@ -1,5 +1,7 @@
 resource "aws_cloudwatch_metric_alarm" "unauthorized_api_calls_alarm" {
-  alarm_name          = "${aws_cloudwatch_log_metric_filter.unauthorized_api_calls_metric.name}-alarm"
+  alarm_name = "${aws_cloudwatch_log_metric_filter.unauthorized_api_calls_metric.name}-alarm"
+  #   temporarily disabled for wiz trial - re-enable in Jan 2025
+  actions_enabled     = false
   alarm_description   = "There have been >3 unauthorized calls to the AWS API in the last minute"
   namespace           = "CISBenchmark"
   comparison_operator = "GreaterThanOrEqualToThreshold"
