500d07ac159da0d4c9647d4b408e5f5f0798821d
Alice Lui <54395483+al01010@users.noreply.github.com>
2025-01-13 10:14:48 +0000

    EPBR-8100: Switch off cloudtrail insights
    In all environments

--- service-infrastructure/logging/cloudtrail.tf
+++ service-infrastructure/logging/cloudtrail.tf
@@ -10,10 +10,4 @@ resource "aws_cloudtrail" "this" {
   is_multi_region_trail         = true
   cloud_watch_logs_group_arn    = "${aws_cloudwatch_log_group.cloudtrail.arn}:*"
   cloud_watch_logs_role_arn     = aws_iam_role.cloudtrail.arn
-  insight_selector {
-    insight_type = "ApiErrorRateInsight"
-  }
-  insight_selector {
-    insight_type = "ApiCallRateInsight"
-  }
 }
