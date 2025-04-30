292efe969f3cc4aee5f3f690eb90e04a601cb2f5
Pedro Alvarez <pedro.alvarez@madetech.com>
2025-04-30 14:13:58 +0100

    OD-115: Improve Glue Catalog Table creation
    We now send the parquet files to a different location
    in the bucket, and then configure the Table to use
    the new location.

--- service-infrastructure/glue/etl_scripts/create_data_catalog.py
+++ service-infrastructure/glue/etl_scripts/create_data_catalog.py
@@ -1,4 +1,7 @@
+import boto3
 import sys
+
+from pathlib import Path
 from awsglue.transforms import *
 from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
@@ -9,6 +12,7 @@ from awsgluedq.transforms import EvaluateDataQuality
 args = getResolvedOptions(sys.argv, ['JOB_NAME', 'DATABASE_NAME', 'S3_BUCKET', 'CONNECTION_NAME', 'CATALOG_TABLE_NAME', "DB_TABLE_NAME"])
 sc = SparkContext()
 glueContext = GlueContext(sc)
+logger = glueContext.get_logger()
 spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
@@ -25,7 +29,34 @@ S3_BUCKET = args['S3_BUCKET']
 CONNECTION_NAME = args['CONNECTION_NAME']
 CATALOG_TABLE_NAME = args['CATALOG_TABLE_NAME']
 DB_TABLE_NAME = args['DB_TABLE_NAME']
-S3_OUTPUT_PATH=f"s3://{S3_BUCKET}/{CATALOG_TABLE_NAME}/"
+
+possible_locations = [f"{CATALOG_TABLE_NAME}_{i}" for i in ["A", "B"]]
+
+glue = boto3.client('glue')
+
+try:
+    table_response = glue.get_table(DatabaseName=DATABASE_NAME, Name=CATALOG_TABLE_NAME)
+    table_info = table_response['Table']
+except glue.exceptions.EntityNotFoundException:
+    logger.warn(f"Table \"{CATALOG_TABLE_NAME}\" not found")
+    table_info = {}
+
+if table_info:
+    current_s3_location = table_info['StorageDescriptor']['Location']
+    logger.warn(f"Current S3 location for table {CATALOG_TABLE_NAME}: {current_s3_location}")
+    current_location = Path(current_s3_location).name
+    try:
+        next_location_index = (possible_locations.index(current_location) + 1) % (len(possible_locations))
+    except ValueError:
+        next_location_index = 0
+else:
+    next_location_index = 0
+
+next_location = possible_locations[next_location_index]
+
+S3_OUTPUT_PATH=f"s3://{S3_BUCKET}/{next_location}/"
+
+logger.warn(f"Data location in S3 for table \"{CATALOG_TABLE_NAME}\": {S3_OUTPUT_PATH}")
 
 # Script generated for node PostgreSQL
 PostgreSQL_node1738584178209 = glueContext.create_dynamic_frame.from_options(
@@ -44,4 +75,29 @@ AmazonS3_node1738584350700 = glueContext.getSink(path=S3_OUTPUT_PATH, connection
 AmazonS3_node1738584350700.setCatalogInfo(catalogDatabase=DATABASE_NAME,catalogTableName=CATALOG_TABLE_NAME)
 AmazonS3_node1738584350700.setFormat("glueparquet", compression="snappy")
 AmazonS3_node1738584350700.writeFrame(PostgreSQL_node1738584178209)
+
+# Switch Glue Table location only if the table was present
+if table_info:
+    logger.warn("Updating Location on glue table")
+
+    # Switch over to the new S3 location
+    table_info['StorageDescriptor']['Location'] = S3_OUTPUT_PATH
+    glue.update_table(
+        DatabaseName=DATABASE_NAME,
+        TableInput={
+            'Name': table_info['Name'],
+            'StorageDescriptor': table_info['StorageDescriptor'],
+            'PartitionKeys': table_info.get('PartitionKeys', []),
+            'TableType': table_info.get('TableType', 'EXTERNAL_TABLE'),
+            'Parameters': table_info.get('Parameters', {})
+        }
+    )
+    logger.warn(f"Location updated, deleting old S3 bucket directory \"{current_location}\"")
+
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket(S3_BUCKET)
+    bucket.objects.filter(Prefix=f"{current_location}/").delete()
+
+    logger.warn("S3 bucket directory removed")
+
 job.commit()
