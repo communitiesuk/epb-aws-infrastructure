0c6323ca7f0b4642ce44769dcec1da7f2b4f1c56
Pedro Alvarez <pedro.alvarez@madetech.com>
2025-05-01 16:13:28 +0100

    OD-107: Create domestic_rr Glue Table

--- service-infrastructure/glue/etl.tf
+++ service-infrastructure/glue/etl.tf
@@ -15,6 +15,23 @@ module "create_domestic_etl" {
   }
 }
 
+module "create_domestic_rr_etl" {
+  source           = "./etl_job"
+  bucket_name      = aws_s3_bucket.this.bucket
+  glue_connector   = [aws_glue_connection.this.name]
+  job_name         = "Create domestic rr catalog table"
+  role_arn         = aws_iam_role.glueServiceRole.arn
+  script_file_name = "create_data_catalog.py"
+  scripts_module   = path.module
+  arguments = {
+    "--DATABASE_NAME"      = aws_glue_catalog_database.this.name
+    "--CATALOG_TABLE_NAME" = "domestic_rr"
+    "--S3_BUCKET"          = aws_s3_bucket.this.bucket
+    "--CONNECTION_NAME"    = aws_glue_connection.this.name
+    "--DB_TABLE_NAME"      = "mvw_domestic_rr_search"
+  }
+}
+
 module "export_domestic_data_by_year" {
   source           = "./etl_job"
   bucket_name      = aws_s3_bucket.this.bucket
