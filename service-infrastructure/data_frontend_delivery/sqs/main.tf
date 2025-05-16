e83be216fce6ff071f4c0ce7768b96613dd51e40
Pedro Alvarez <pedro.alvarez@madetech.com>
2025-05-16 15:48:00 +0100

    OD-52: Upgrade aws provider

--- service-infrastructure/data_frontend_delivery/sqs/main.tf
+++ service-infrastructure/data_frontend_delivery/sqs/main.tf
@@ -3,7 +3,7 @@ terraform {
 
   required_providers {
     aws = {
-      version = "~>5.63"
+      version = "~>5.98"
       source  = "hashicorp/aws"
     }
     archive = {
--- service-infrastructure/main.tf
+++ service-infrastructure/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = "~>1.3"
   required_providers {
     aws = {
-      version = "~>5.63"
+      version = "~>5.98"
       source  = "hashicorp/aws"
     }
   }
