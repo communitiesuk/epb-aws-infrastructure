759ed421bcf5b69727e989764da7d354062a8568
Kev Keenoy <kevin.keenoy@communities.gov.uk>
2025-07-24 09:27:31 +0100

    Remove now-unused PG-14 serverless parameter group
    All warehouse instances are now on PG-17.x so we don't need this old parameter group.

--- service-infrastructure/database_parameter_groups/outputs.tf
+++ service-infrastructure/database_parameter_groups/outputs.tf
@@ -2,14 +2,10 @@ output "aurora_pg_param_group_name" {
   value = aws_rds_cluster_parameter_group.rds_aurora.name
 }
 
-output "aurora_pg_serverless_param_group_name" {
-  value = aws_rds_cluster_parameter_group.rds_aurora_serverless.name
+output "aurora_pg_17_serverless_param_group_name" {
+  value = aws_rds_cluster_parameter_group.rds_aurora_serverless_17.name
 }
 
 output "rds_pg_param_group_name" {
   value = try(aws_db_parameter_group.rds_db[0].name, "")
-}
-
-output "aurora_pg_17_serverless_param_group_name" {
-  value = aws_rds_cluster_parameter_group.rds_aurora_serverless_17.name
 }
