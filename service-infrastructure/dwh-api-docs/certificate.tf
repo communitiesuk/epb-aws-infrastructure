327ed3db64b78123667934e0b2be918f805772b1
Barry Halper <69764062+barryhalper@users.noreply.github.com>
2025-10-20 12:00:22 +0100

    Update certificate sub-domain to api-docs.
    Users will know what 'dwh' means so removed it from the subdomain

--- service-infrastructure/dwh-api-docs/certificate.tf
+++ service-infrastructure/dwh-api-docs/certificate.tf
@@ -1,5 +1,5 @@
 resource "aws_acm_certificate" "cert-cdn" {
-  domain_name       = "dwh-api-docs.${var.domain_name}"
+  domain_name       = "api-docs.${var.domain_name}"
   validation_method = "DNS"
   lifecycle {
     create_before_destroy = true
