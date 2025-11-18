4ff498155974530b349387abe707a7b44674f02b
Aleksandra Ivanova <aleksandra98iv@gmail.com>
2025-11-18 09:40:05 +0000

    OD-290: Refactors Iceberg catalog population script
    - Enhances argument parsing and schema synchronization with PostgreSQL, so when a new column is added or removed from the mvw catalog is dynamically updated.

--- service-infrastructure/glue_data_warehouse/etl_scripts/populate_iceberg_catalog.py
+++ service-infrastructure/glue_data_warehouse/etl_scripts/populate_iceberg_catalog.py
@@ -8,11 +8,25 @@ from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
 from awsgluedq.transforms import EvaluateDataQuality
-
 from pyspark.sql import SparkSession
 
-args = getResolvedOptions(sys.argv, ['JOB_NAME', 'DATABASE_NAME', 'S3_BUCKET', 'CONNECTION_NAME', 'CATALOG_TABLE_NAME', "DB_TABLE_NAME", "COLUMNS"])
+# -------------------------------------------------------
+#               PARSE JOB ARGUMENTS
+# -------------------------------------------------------
+args = getResolvedOptions(sys.argv, ['JOB_NAME', 'DATABASE_NAME', 'S3_BUCKET', 'CONNECTION_NAME', 'CATALOG_TABLE_NAME', "DB_TABLE_NAME"])
 
+DATABASE_NAME =  args['DATABASE_NAME']
+S3_BUCKET = args['S3_BUCKET']
+CONNECTION_NAME = args['CONNECTION_NAME']
+CATALOG_TABLE_NAME = args['CATALOG_TABLE_NAME']
+DB_TABLE_NAME = args['DB_TABLE_NAME']
+
+S3_PATH=f"s3://{S3_BUCKET}/{CATALOG_TABLE_NAME}/"
+GLUE_TABLE_PATH = f"glue_catalog.{DATABASE_NAME}.{CATALOG_TABLE_NAME}"
+
+# -------------------------------------------------------
+#               SETUP SPARK + GLUE CONTEXT
+# -------------------------------------------------------
 sc = SparkContext()
 glueContext = GlueContext(sc)
 logger = glueContext.get_logger()
@@ -20,20 +34,12 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
-# Default ruleset used by all target nodes with data quality enabled
-DEFAULT_DATA_QUALITY_RULESET = """
-    Rules = [
-        ColumnCount > 0
-    ]
-"""
+logger.info("Glue job initialized successfully.")
 
-DATABASE_NAME =  args['DATABASE_NAME']
-S3_BUCKET = args['S3_BUCKET']
-CONNECTION_NAME = args['CONNECTION_NAME']
-CATALOG_TABLE_NAME = args['CATALOG_TABLE_NAME']
-DB_TABLE_NAME = args['DB_TABLE_NAME']
-S3_PATH=f"s3://{S3_BUCKET}/{CATALOG_TABLE_NAME}/"
-COLUMNS =  args['COLUMNS']
+# -------------------------------------------------------
+#        INITIALIZE ICEBERG CATALOG IF NECESSARY
+# -------------------------------------------------------
+logger.info("Creating Iceberg table if it does not exist.")
 
 sql_spark = (
     SparkSession.builder
@@ -45,18 +51,21 @@ sql_spark = (
 )
 
 sql_spark.sql(f"""
-  CREATE TABLE IF NOT EXISTS glue_catalog.{DATABASE_NAME}.{CATALOG_TABLE_NAME}(
-      {COLUMNS}
-)
-USING iceberg
-LOCATION '{S3_PATH}'
-TBLPROPERTIES (
-  'write.format.default' = 'parquet',
-  'write.compression.codec' = 'zstd',
-  'optimize_rewrite_delete_file_threshold'='5'
-)
+  CREATE TABLE IF NOT EXISTS {GLUE_TABLE_PATH}
+  USING iceberg
+  LOCATION '{S3_PATH}'
+  TBLPROPERTIES (
+    'write.format.default' = 'parquet',
+    'write.compression.codec' = 'zstd',
+    'optimize_rewrite_delete_file_threshold'='5'
+ )
 """);
 
+logger.info("Iceberg table ensured.")
+
+# -------------------------------------------------------
+#             CONFIGURE TABLE OPTIMIZERS
+# -------------------------------------------------------
 sts = boto3.client("sts")
 iam = boto3.client("iam")
 glue = boto3.client("glue")
@@ -92,7 +101,6 @@ optimizer_configurations = {
 
 
 for optimizer_type in optimizer_configurations.keys():
-
     try:
         logger.warn(f"Trying to create optimizer for {optimizer_type}")
 
@@ -111,46 +119,94 @@ for optimizer_type in optimizer_configurations.keys():
     except glue.exceptions.AlreadyExistsException:
         logger.warn(f"Table optimizer of type {optimizer_type} already present")
 
+# -------------------------------------------------------
+#       EXTRACT DATA FROM POSTGRESQL SOURCE
+# -------------------------------------------------------
+logger.info("Reading source table from PostgreSQL.")
 
-
-# Script generated for node PostgreSQL
-PostgreSQL_node1748526875522 = glueContext.create_dynamic_frame.from_options(
+PostgreSQL_src = glueContext.create_dynamic_frame.from_options(
     connection_type = "postgresql",
     connection_options = {
         "useConnectionProperties": "true",
         "dbtable": DB_TABLE_NAME,
         "connectionName": CONNECTION_NAME,
     },
-    transformation_ctx = "PostgreSQL_node1748526875522"
+    transformation_ctx = "PostgreSQL_src"
 )
 
-# Script generated for node AWS Glue Data Catalog
-AWSGlueDataCatalog_node1748526922140_df = glueContext.create_data_frame.from_catalog(database=DATABASE_NAME, table_name=CATALOG_TABLE_NAME)
-
-postgres_df = PostgreSQL_node1748526875522.toDF()
+postgres_df = PostgreSQL_src.toDF()
 postgres_df.createOrReplaceTempView(DB_TABLE_NAME)
 
-columns = [f.name for f in spark.table(DB_TABLE_NAME).schema.fields]
+logger.info(f"Loaded PostgreSQL table: {DB_TABLE_NAME}")
+
+# -------------------------------------------------------
+#  DYNAMICALLY UPDATE GLUE TABLE COLUMN DEFINITIONS
+# -------------------------------------------------------
+def get_postgres_columns_and_types(spark, postgres_table_name):
+    postgres_table_schema = spark.table(postgres_table_name).schema
+    postgres_columns_with_types = {f.name: f.dataType.simpleString() for f in postgres_table_schema.fields}
+    return postgres_columns_with_types
+
+def dynamically_update_catalog_table(glue_spark, postgres_columns_with_types, GLUE_TABLE_PATH):
+    logger.info("Synchronizing Iceberg table schema with PostgreSQL schema.")
+    postgres_columns = postgres_columns_with_types.keys()
+
+    glue_table_schema = glue_spark.table(GLUE_TABLE_PATH).schema
+    glue_columns = [f.name for f in glue_table_schema.fields]
+
+    # Identify new columns
+    new_columns = set(postgres_columns) - set(glue_columns)
+    for column_name in new_columns:
+        col_type = postgres_columns_with_types[column_name]
+        logger.info(f"Adding new column → {column_name}: {col_type}")
+
+        glue_spark.sql(f"""
+            ALTER TABLE {GLUE_TABLE_PATH}
+            ADD COLUMN {column_name} {col_type}
+        """)
+
+    # Identify columns to drop
+    drop_columns = set(glue_columns) - set(postgres_columns)
+    for column_name in drop_columns:
+        logger.info(f"Dropping column → {column_name}")
+        glue_spark.sql(f"""
+          ALTER TABLE {GLUE_TABLE_PATH}
+          DROP COLUMN {column_name}
+          """
+    )
+
+
+postgres_columns_with_types = get_postgres_columns_and_types(spark, DB_TABLE_NAME)
+columns = postgres_columns_with_types.keys()
+
+dynamically_update_catalog_table(sql_spark, postgres_columns_with_types, GLUE_TABLE_PATH)
+
+# -------------------------------------------------------
+#      PERFORM ICEBERG MERGE INTO TABLE
+# -------------------------------------------------------
+logger.info("Performing MERGE INTO operation on Iceberg table.")
+
 column_list = ", ".join(columns)
 value_list = ", ".join([f"source.{col}" for col in columns])
 
 if "_rr" in CATALOG_TABLE_NAME:
     spark.sql(f"""
-    MERGE INTO glue_catalog.{DATABASE_NAME}.{CATALOG_TABLE_NAME} AS target
-    USING {DB_TABLE_NAME} AS source 
-    ON target.certificate_number = source.certificate_number 
-    WHEN NOT MATCHED THEN 
+    MERGE INTO {GLUE_TABLE_PATH} AS target
+    USING {DB_TABLE_NAME} AS source
+    ON target.certificate_number = source.certificate_number
+    WHEN NOT MATCHED THEN
         INSERT ({column_list}) VALUES ({value_list})
     """)
 else:
     spark.sql(f"""
-    MERGE INTO glue_catalog.{DATABASE_NAME}.{CATALOG_TABLE_NAME} AS target
+    MERGE INTO {GLUE_TABLE_PATH} AS target
     USING {DB_TABLE_NAME} AS source
     ON target.certificate_number = source.certificate_number
-    WHEN MATCHED 
+    WHEN MATCHED
                 THEN UPDATE SET *
     WHEN NOT MATCHED THEN
         INSERT ({column_list}) VALUES ({value_list})
     """)
 
 job.commit()
+logger.info("Glue job completed successfully.")
