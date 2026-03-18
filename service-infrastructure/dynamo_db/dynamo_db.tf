3852ae506b887cb77452b90a2823449f575d71a4
Barry Halper <69764062+barryhalper@users.noreply.github.com>
2026-03-18 15:19:56 +0000

    Updates Dynamo db to use an autoscaling policy for user credentials
    Resolves the 'level of configured provisioned throughput for the table was exceeded' error

--- service-infrastructure/dynamo_db/dynamo_db.tf
+++ service-infrastructure/dynamo_db/dynamo_db.tf
@@ -26,6 +26,10 @@ resource "aws_dynamodb_table" "this" {
 
   lifecycle {
     prevent_destroy = true
+    ignore_changes = [
+      read_capacity,
+      write_capacity,
+    ]
   }
 
 }
@@ -40,3 +44,58 @@ resource "aws_vpc_endpoint" "this" {
 
   route_table_ids = var.route_table_ids
 }
+
+locals {
+  read_max_capacity  = var.environment == "prod" ? 20000 : 20
+  write_max_capacity = var.environment == "prod" ? 1000 : 20
+}
+
+
+resource "aws_appautoscaling_target" "user_credentials_table_read_target" {
+  max_capacity       = local.read_max_capacity
+  min_capacity       = 1
+  resource_id        = "table/${aws_dynamodb_table.this.name}"
+  scalable_dimension = "dynamodb:table:ReadCapacityUnits"
+  service_namespace  = "dynamodb"
+}
+
+resource "aws_appautoscaling_target" "user_credentials_table_write_target" {
+  max_capacity       = local.write_max_capacity
+  min_capacity       = 1
+  resource_id        = "table/${aws_dynamodb_table.this.name}"
+  scalable_dimension = "dynamodb:table:WriteCapacityUnits"
+  service_namespace  = "dynamodb"
+}
+
+resource "aws_appautoscaling_policy" "user_credentials_table_read_policy" {
+  name               = "DynamoDBReadCapacityUtilization:${aws_appautoscaling_target.user_credentials_table_read_target.resource_id}"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.user_credentials_table_read_target.resource_id
+  scalable_dimension = aws_appautoscaling_target.user_credentials_table_read_target.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.user_credentials_table_read_target.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "DynamoDBReadCapacityUtilization"
+    }
+
+    target_value = 70
+  }
+}
+
+resource "aws_appautoscaling_policy" "user_credentials_table_write_policy" {
+  name               = "DynamoDBWriteCapacityUtilization:${aws_appautoscaling_target.user_credentials_table_write_target.resource_id}"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.user_credentials_table_write_target.resource_id
+  scalable_dimension = aws_appautoscaling_target.user_credentials_table_write_target.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.user_credentials_table_write_target.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "DynamoDBWriteCapacityUtilization"
+    }
+
+    target_value = 70
+  }
+}
+
