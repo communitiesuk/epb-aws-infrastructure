9ebca74ec2e9e37998d2aa988d64d4ac0baa0033
Kev Keenoy <kevin.keenoy@communities.gov.uk>
2026-05-07 17:11:00 +0100

    Update bastion host to run new LTS Ubuntu "Resolute Raccoon"

--- service-infrastructure/bastion/bastion.tf
+++ service-infrastructure/bastion/bastion.tf
@@ -37,7 +37,7 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-resolute-26.04-amd64-server-*"]
   }
 
   filter {
