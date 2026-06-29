9011302365e0f16d0a1874e8e53a87e737894839
Pedro Alvarez <pedro.alvarez@madetech.com>
2026-06-29 16:24:18 +0100

    OD-400: Create step functions automations
    This is to automate the process of:
    - Materialized view refresh tasks (including recommendations)
    - Populate Jobs (including recommendations)
    - Delete Job
    - Generate Zip file export with CSV files

---
+++ service-infrastructure/glue_data_warehouse/step_functions.tf
@@ -0,0 +1,61 @@
+module "display_refresh_orchestration" {
+  source = "./step_functions"
+  prefix = "${var.prefix}-display"
+
+  ecs_cluster_arn               = var.ecs_cluster_arn
+  ecs_task_definition_arn       = var.ecs_task_exec_arn
+  ecs_container_name            = var.ecs_migration_container_name
+  ecs_subnet_ids                = var.ecs_subnet_ids
+  ecs_security_group_id         = var.ecs_security_group_id
+  ecs_task_role_arn             = var.ecs_task_role_arn
+  ecs_task_execution_role_arn   = var.ecs_task_execution_role_arn
+  ecs_materialized_view_name    = "mvw_dec_search"
+  ecs_rr_materialized_view_name = "mvw_dec_rr_search"
+
+  glue_populate_job_name    = module.populate_dec_etl.etl_job_name
+  glue_populate_rr_job_name = module.populate_dec_rr_etl.etl_job_name
+  glue_delete_job_name      = module.delete_iceberg_data.etl_job_name
+  glue_zip_export_job_name  = module.export_dec_data_by_year.etl_job_name
+}
+
+module "non_domestic_refresh_orchestration" {
+  source = "./step_functions"
+  prefix = "${var.prefix}-non_domestic"
+
+  ecs_cluster_arn               = var.ecs_cluster_arn
+  ecs_task_definition_arn       = var.ecs_task_exec_arn
+  ecs_container_name            = var.ecs_migration_container_name
+  ecs_subnet_ids                = var.ecs_subnet_ids
+  ecs_security_group_id         = var.ecs_security_group_id
+  ecs_task_role_arn             = var.ecs_task_role_arn
+  ecs_task_execution_role_arn   = var.ecs_task_execution_role_arn
+  ecs_materialized_view_name    = "mvw_commercial_search"
+  ecs_rr_materialized_view_name = "mvw_commercial_rr_search"
+
+  glue_populate_job_name    = module.populate_non_domestic_etl.etl_job_name
+  glue_populate_rr_job_name = module.populate_non_domestic_rr_etl.etl_job_name
+
+  glue_delete_job_name     = module.delete_iceberg_data.etl_job_name
+  glue_zip_export_job_name = module.export_non_domestic_data_by_year.etl_job_name
+}
+
+module "domestic_refresh_orchestration" {
+  source = "./step_functions"
+  prefix = "${var.prefix}-domestic"
+
+  ecs_cluster_arn               = var.ecs_cluster_arn
+  ecs_task_definition_arn       = var.ecs_task_exec_arn
+  ecs_container_name            = var.ecs_migration_container_name
+  ecs_subnet_ids                = var.ecs_subnet_ids
+  ecs_security_group_id         = var.ecs_security_group_id
+  ecs_task_role_arn             = var.ecs_task_role_arn
+  ecs_task_execution_role_arn   = var.ecs_task_execution_role_arn
+  ecs_materialized_view_name    = "mvw_domestic_search"
+  ecs_rr_materialized_view_name = "mvw_domestic_rr_search"
+
+  glue_populate_job_name    = module.populate_domestic_etl.etl_job_name
+  glue_populate_rr_job_name = module.populate_domestic_rr_etl.etl_job_name
+
+  glue_delete_job_name     = module.delete_iceberg_data.etl_job_name
+  glue_zip_export_job_name = module.export_domestic_data_by_year.etl_job_name
+}
