aadf6710c472f4e5eb1ac5c59ea8d86c0feb721d
kaylummccann-mt <kaylum.mccann@madetech.com>
2026-07-06 11:50:32 +0100

    EPBR-9468: Change glue_data_warehouse catalog_start_year to 2011

--- service-infrastructure/glue_data_warehouse/main.tf
+++ service-infrastructure/glue_data_warehouse/main.tf
@@ -10,7 +10,7 @@ terraform {
 }
 
 locals {
-  catalog_start_year = 2012
+  catalog_start_year = 2011
   catalog_end_year   = 2026 #Change to the current year if the catalog needs to be repopulated
   number_years       = (local.catalog_end_year - local.catalog_start_year)
   number_of_jobs     = local.number_years + 1
