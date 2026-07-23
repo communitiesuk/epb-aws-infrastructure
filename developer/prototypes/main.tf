6096d031126f85e1a2092754e86c9d2486f9c762
Kev Keenoy <kevin.keenoy@communities.gov.uk>
2026-07-23 15:53:42 +0100

    Remove unused provider blocks (flagged by tflint)

--- developer/prototypes/main.tf
+++ developer/prototypes/main.tf
@@ -14,8 +14,3 @@ provider "aws" {
   shared_config_files      = ["~/.aws/config"]
   shared_credentials_files = ["~/.aws/credentials"]
 }
-
-provider "aws" {
-  alias  = "us-east"
-  region = "us-east-1"
-}
